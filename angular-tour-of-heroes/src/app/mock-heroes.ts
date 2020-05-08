9ac36fa5b0e9c774c816ac53ec06d7a376b406d3
Josh <Joshua@Joshua-Dickens.com>
2020-05-08 15:06:30 -0700

    Heroes list and selector

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Deku'},
+  { id: 12, name: 'Nighteye'},
+  { id: 13, name: 'Lemillion'},
+  { id: 14, name: 'Eraserhead'},
+  { id: 15, name: 'All Might'},
+  { id: 16, name: 'Bakugo'},
+  { id: 17, name: 'Iida'},
+  { id: 18, name: 'Jiro'},
+  { id: 19, name: 'Ururaka'},
+  { id: 20, name: 'Kendo'}
+]
